3236e14a2aa117477ff624647a370da8e88e1d76
Naokichi <naokichi.tany@gmail.com>
2021-02-09 15:53:01 +0900

    Dynamic routesに変更

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 const Post = ({ post }) => {
   return (
     <div>
       <span>{post.id}</span>
       {" : "}
-      <span href={`/posts/${post.id}`}>
+      <Link href={`/posts/${post.id}`}>
         <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
           {post.title}
         </span>
-      </span>
+      </Link>
     </div>
   );
 };
